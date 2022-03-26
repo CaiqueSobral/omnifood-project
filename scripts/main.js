7f4d9e48b1186d8d384920b1e9a8f45fdc58db84
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2022-03-26 13:33:32 -0300

    Feat: Add the project to github

---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+// Set current year to copyright
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// create the mobile navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to the top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close the mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
---
+++ styles/rules.md
@@ -0,0 +1,65 @@
+## --- 01 TYPOGRAPHY SYSTEM
+
+# - Font sizes (px) -
+
+10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98
+
+# - Font weights -
+
+Default: 400
+Medium: 500
+Semi-Bold: 600
+Bold: 700
+
+# - Line heights -
+
+Default: 1
+Small: 1.05
+Medium: 1.2
+Large: 1.8
+Paragraph: 1.6
+
+# - Letter Spacing -
+
+-0.5px
+0.75px
+
+## --- 02 COLORS
+
+Primary:
+#e67e22
+
+- tints:
+  #fdf2e9
+  #fae5d3
+  #eb984e
+
+- shades:
+  #cf711f
+  #45260a
+
+# - Accents:
+
+#51cf66
+
+# - Greys:
+
+#888
+#767676
+#6f6f6f
+#555
+#333
+
+## --- 05 SHADOWS
+
+0 2.4rem 4.8rem rgba(0, 0, 0, 0.075)
+
+## --- 06 BORDER RADIUS
+
+Default: 9px
+Medium-High: 11px
+
+## --- 07 WHITESPACE
+
+- Spacing (px)
+  2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128
